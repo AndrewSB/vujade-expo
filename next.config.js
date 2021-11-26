576620dca37674f4ce9901899737e70c9522289c
Andrew Breckenridge <asbreckenridge@me.com>
2021-11-26 11:05:10 -0800

    WIP: downgrades to next^11 for tamagui

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 const withPlugins = require('next-compose-plugins');
 const { withTamagui } = require('@tamagui/next-plugin');
 
-module.exports = withPlugins(
-  withTamagui({
-    config: './tamagui.config.ts',
-    components: ['tamagui'],
-    importsWhitelist: ['constants.js', 'colors.js'],
-    logTimings: true,
-    disableExtraction: process.env.NODE_ENV === 'development',
-  })
-)
+module.exports = withPlugins([
+    withTamagui({
+        components: [],
+    })
+]);
+
+// module.exports = withPlugins(
+//   [withTamagui({
+//     config: './tamagui.config.ts',
+//     components: ['tamagui'],
+//     importsWhitelist: [],
+//     logTimings: true,
+//     disableExtraction: process.env.NODE_ENV === 'development',
+//   })
+// ]
+// )
